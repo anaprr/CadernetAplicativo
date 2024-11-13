9603a45830eb58154d595a0c304fb912a65ae1c1
anaalturria <anavxsss@gmail.com>
2024-11-13 13:56:01 -0300

    envia

--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -3,28 +3,34 @@ import { createContext, useState } from "react";
 export const AuthContext = createContext(0);
 
 function AuthProvider({ children }) {   
-    const [logado, setLogado] = useState(true);
+    const [logado, setLogado] = useState(false);
     const [error, setError] = useState(false);
     const [cadastro, setCadastro] = useState(false);
+    const [novaobs, setNovaobs ] = useState( false );
+    const [ usuario, setUsuario ] = useState();
+
 
     async function Login(email, senha) {
         if (email && senha) {
-            await fetch('http://10.139.75.101:5251/api/Usuarios/Login', {
-
+            await fetch('http://10.139.75.53:5251/api/Usuarios/Login', {
                 method: 'POST',
                 headers: {
-                    'Content-Type': 'application/json',
+                    'content-type': 'application/json'
                 },
                 body: JSON.stringify({
-                    UsuarioEmail: email,
-                    UsuarioSenha: senha,
-                }),
+                    usuarioEmail: email,
+                    usuarioSenha: senha
+                })
             })
-            .then((res) => res.json())
-            .then((json) => {
-                setLogado(json ? true : false);
-                setError(!json);  
-            })
+            .then( res => res.json())
+            .then( json  => {
+                if(json.usuarioId) {
+                    setLogado( true );
+                    setUsuario( json );
+                } else {
+                    setError( true );
+                }
+            } )
             .catch((err) => {
                 console.error("Erro ao realizar login:", err);
                 setError(true);
@@ -35,7 +41,7 @@ function AuthProvider({ children }) {
     }
 
     return (
-        <AuthContext.Provider value={{ logado, Login, error, cadastro, setCadastro }}>
+        <AuthContext.Provider value={{ logado, Login, error, cadastro, setCadastro, novaobs: novaobs, setNovaobs, usuario: usuario  }}>
             {children}
         </AuthContext.Provider>
     );
